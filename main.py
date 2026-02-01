1dea26976bb598db184573b66fcec671de5df7fc
dcb2088 <dcb2088@gmail.com>
2026-02-01 14:06:31 -0500

    add player ship with rotation controls

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -9,12 +10,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        dt = clock.tick(60) / 1000 # Limit to 60 FPS
+        player.update(dt) # Update the player
+        player.draw(screen) # Draw the player
         pygame.display.flip()
 
 if __name__ == "__main__":
